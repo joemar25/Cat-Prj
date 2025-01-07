c0a56ac00ddae0b439652859d9d7e499ae93f402
tat_on_sweater <bedisscottandrew@gmail.com>
2025-01-07 08:53:58 +0800

    fix: added the 3 dots button in data table-row-actions

--- src/components/custom/users/data-table-row-actions.tsx
+++ src/components/custom/users/data-table-row-actions.tsx
@@ -99,22 +99,7 @@ export function DataTableRowActions({
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant='ghost' className='h-8 w-8 p-0'>
-            <svg
-              className='h-4 w-4 dark:text-gray-100 text-gray-800'
-              xmlns='http://www.w3.org/2000/svg'
-              width='24'
-              height='24'
-              viewBox='0 0 24 24'
-              fill='currentColor'
-              stroke='currentColor'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-            >
-              <circle cx='5' cy='12' r='1' />
-              <circle cx='12' cy='12' r='1' />
-              <circle cx='19' cy='12' r='1' />
-            </svg>
+            <Icons.moreHorizontal className='h-4 w-4' />
             <span className='sr-only'>Open menu</span>
           </Button>
         </DropdownMenuTrigger>
