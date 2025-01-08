4991f74955972a1303b9f649ba94692beb1f8917
Alro John <alromercado08@gmail.com>
2025-01-08 17:22:02 +0800

    forms and annotations

--- src/app/dummyForm/page.tsx
+++ src/app/dummyForm/page.tsx
@@ -7,12 +7,16 @@ import DeathCertificateForm from "@/components/custom/forms/death-form";
 import MarriageCertificateForm from "@/components/custom/forms/marriage-form";
 import  RequestCertificatesModal  from "@/components/custom/modals/request-certificates-modal"; // Corrected import
 import MarriageAnnotationForm from "@/components/custom/annotation/marriage";
+import DeathAnnotationForm from "@/components/custom/annotation/death";
+import BirthAnnotationForm from "@/components/custom/annotation/birthcert";
 
 export default function DummyForm() {
   const [isBirthModalOpen, setIsBirthModalOpen] = useState(false);
   const [isDeathModalOpen, setIsDeathModalOpen] = useState(false);
   const [isMarriageModalOpen, setIsMarriageModalOpen] = useState(false);
   const [isMarriageAnnotationModalOpen, setIsMarriageAnnotationModalOpen] = useState(false);
+  const [isDeathAnnotationModalOpen, setIsDeathAnnotationModalOpen] = useState(false);
+  const [isBirthAnnotationModalOpen, setIsBirthAnnotationModalOpen] = useState(false);
 
   return (
     <div>
@@ -21,6 +25,8 @@ export default function DummyForm() {
         <button onClick={() => setIsDeathModalOpen(true)}>Death Certificate</button>
         <button onClick={() => setIsMarriageModalOpen(true)}>Marriage Certificate</button>
         <button onClick={() => setIsMarriageAnnotationModalOpen(true)}>Marriage Annotation</button>
+        <button onClick={() => setIsDeathAnnotationModalOpen(true)}>Death Annotation</button>
+        <button onClick={() => setIsBirthAnnotationModalOpen(true)}>Birth Annotation</button>
       </div>
 
       {/* Birth Certificate Modal */}
@@ -41,6 +47,16 @@ export default function DummyForm() {
       {/* Marriage Annotation Modal */}
       <RequestCertificatesModal isOpen={isMarriageAnnotationModalOpen} onClose={() => setIsMarriageAnnotationModalOpen(false)}>
         <MarriageAnnotationForm />
+      </RequestCertificatesModal>
+
+      {/* Death Annotation Modal */}
+      <RequestCertificatesModal isOpen={isDeathAnnotationModalOpen} onClose={() => setIsDeathAnnotationModalOpen(false)}>
+        <DeathAnnotationForm />
+      </RequestCertificatesModal>
+
+      {/* Birth Annotation Modal */}
+      <RequestCertificatesModal isOpen={isBirthAnnotationModalOpen} onClose={() => setIsBirthAnnotationModalOpen(false)}>
+        <BirthAnnotationForm />
       </RequestCertificatesModal>
     </div>
   );
