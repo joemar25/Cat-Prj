c1f914ad74023625303219fda494787cf290e3ac
Alro John <alromercado08@gmail.com>
2025-01-24 00:08:05 +0800

    input border done, basic search and name search done

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
       <input
         type={type}
         className={cn(
-          "ring-none outline-none flex w-full rounded-md border border-black/20 dark:border-white/20 bg-transparent p-3 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-1 dark:focus:outline-yellowColor focus:outline-blueColor/50 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "ring-none outline-none flex w-full rounded-md border border-black/30 dark:border-white/20 bg-transparent p-3 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-1 dark:focus:outline-yellowColor focus:outline-blueColor/50 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           className
         )}
         ref={ref}
