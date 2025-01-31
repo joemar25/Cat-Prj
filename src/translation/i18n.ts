bdf644eac8d6480461b1ffd68340b8c14fe364da
Justin Bea <jusrinbea@gmail.com>
2025-01-31 11:29:07 +0800

    remove undefine window

--- src/translation/i18n.ts
+++ src/translation/i18n.ts
@@ -1,27 +1,24 @@
+"use client"
+
 import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
 import en from './en.json'
 import fil from './fil.json'
-
-import { initReactI18next } from 'react-i18next'
-
-const defaultLanguage = typeof window !== 'undefined'
-  ? localStorage.getItem('language') || 'en'
-  : 'en'
 
 const resources = {
   en: { translation: en },
   fil: { translation: fil },
-}
+};
 
-if (!i18n.isInitialized) {
-  i18n.use(initReactI18next).init({
-    resources,
-    lng: defaultLanguage,
-    fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false,
-    },
-  })
-}
+i18n.use(initReactI18next).init({
+  resources,
+  lng: localStorage.getItem('language') || 'en', // Default to English if no language is set in localStorage
+  fallbackLng: 'en', // Fallback to English in case the selected language is unavailable
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
-export default i18n
+export default i18n;
+
+
