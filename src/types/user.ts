811eb814b9a3444de6eb8d5f386ca9bbf1117bba
joemar25 <joemar.cardino000@gmail.com>
2025-02-11 11:33:10 +0800

    Debugging

--- src/types/user.ts
+++ src/types/user.ts
@@ -4,7 +4,12 @@ import { Permission, Profile, Role, User as PrismaUser } from '@prisma/client'
 export interface UserWithRoleAndProfile extends PrismaUser {
     profile: Profile | null
     roles: {
-        role: Role & {
+        role: {
+            id: string
+            name: string
+            description: string | null
+            createdAt: Date
+            updatedAt: Date
             permissions: {
                 permission: Permission
             }[]
